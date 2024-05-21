5eb65f68456882817e2722edffea18b7032ef762
Jorge Luciano <jorgelss912345@gmail.com>
2024-05-20 20:29:14 -0500

    Cars añadidos

---
+++ src/data/dbcars.js
@@ -0,0 +1,92 @@
+export const cars = [
+  {
+    "id": 1,
+    "nombre": "Toyota Corolla",
+    "precio": 40,
+    "ubicacion": "34.052235, -118.243683",
+    "marca": "Toyota",
+    "modelo": "Corolla",
+    "año": 2019
+  },
+  {
+    "id": 2,
+    "nombre": "Honda Civic",
+    "precio": 45,
+    "ubicacion": "34.052235, -118.243683",
+    "marca": "Honda",
+    "modelo": "Civic",
+    "año": 2018
+  },
+  {
+    "id": 3,
+    "nombre": "Ford Focus",
+    "precio": 38,
+    "ubicacion": "34.052235, -118.243683",
+    "marca": "Ford",
+    "modelo": "Focus",
+    "año": 2020
+  },
+  {
+    "id": 4,
+    "nombre": "Chevrolet Malibu",
+    "precio": 50,
+    "ubicacion": "34.140537, -118.387553",
+    "marca": "Chevrolet",
+    "modelo": "Malibu",
+    "año": 2019
+  },
+  {
+    "id": 5,
+    "nombre": "Nissan Altima",
+    "precio": 42,
+    "ubicacion": "34.140537, -118.387553",
+    "marca": "Nissan",
+    "modelo": "Altima",
+    "año": 2021
+  },
+  {
+    "id": 6,
+    "nombre": "BMW 3 Series",
+    "precio": 60,
+    "ubicacion": "34.140537, -118.387553",
+    "marca": "BMW",
+    "modelo": "3 Series",
+    "año": 2019
+  },
+  {
+    "id": 7,
+    "nombre": "Audi A4",
+    "precio": 65,
+    "ubicacion": "34.022348, -118.285117",
+    "marca": "Audi",
+    "modelo": "A4",
+    "año": 2020
+  },
+  {
+    "id": 8,
+    "nombre": "Mercedes-Benz C-Class",
+    "precio": 70,
+    "ubicacion": "34.022348, -118.285117",
+    "marca": "Mercedes-Benz",
+    "modelo": "C-Class",
+    "año": 2018
+  },
+  {
+    "id": 9,
+    "nombre": "Volkswagen Jetta",
+    "precio": 40,
+    "ubicacion": "34.022348, -118.285117",
+    "marca": "Volkswagen",
+    "modelo": "Jetta",
+    "año": 2019
+  },
+  {
+    "id": 10,
+    "nombre": "Hyundai Elantra",
+    "precio": 35,
+    "ubicacion": "34.052235, -118.243683",
+    "marca": "Hyundai",
+    "modelo": "Elantra",
+    "año": 2020
+  }
+]
